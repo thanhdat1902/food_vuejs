ebd99a040e9428bfe9f4bce08b6e194889466131
nhhung1810 <59091317+yauangon@users.noreply.github.com>
2021-04-14 10:45:04 +0700

    add router, change version of scss

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <Header />
-    <Footer />
-    <Body />
-    
-    <!-- <HelloWorld msg="Hoc viec"/> -->
-    
-  </div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <div id="app">
+        <Header />
+        <Footer />
+        <Body />
+      </div>
 
+    </div>
+    <router-view/>
+  </div>
 </template>
 
 <script>
@@ -26,10 +30,7 @@ export default {
 }
 </script>
 
-<style>
-* {
-  margin:0;
-}
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -37,4 +38,20 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueCompositionAPI from '@vue/composition-api'
+import router from './router'
 
 Vue.use(VueCompositionAPI)
 //If you not install the Vue Composition API, please
@@ -12,5 +13,6 @@ Vue.use(VueCompositionAPI)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
